fa545d30217806b99fca24a620ae34f2a3447ec1
khosbilegt <89562265+khosbilegt@users.noreply.github.com>
2022-11-25 14:10:15 +0800

    added graphics pipeline

--- CustomHeaders/PS_Test.hpp
+++ CustomHeaders/PS_Test.hpp
@@ -3,6 +3,9 @@
 #include "PS_Device.hpp"
 #include "PS_Pipeline.hpp"
 #include "PS_Window.hpp"
+#include "PS_SwapChain.hpp"
+
+#include <memory>
 
 namespace ps {
 	class PS_Test {
@@ -10,15 +13,25 @@ namespace ps {
 		static constexpr int WIDTH = 800;
 		static constexpr int HEIGHT = 800;
 
+		PS_Test();
+		~PS_Test();
+
+		PS_Test(const PS_Test&) = delete;
+		PS_Test& operator = (const PS_Test&) = delete;
 		void run();
 
 	private:
+		void createPipelineLayout();
+		void createPipeline();
+		void createCommandBuffers();
+		void drawFrame();
+
 		PS_Window psWindow{WIDTH, HEIGHT, "Hello Vulkan"};
 		PS_Device psDevice{psWindow};
-		PS_Pipeline psPipeline{
-			psDevice, 
-			"Shader/shader.vert.spv", 
-			"Shader/shader.frag.spv",
-			PS_Pipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
+		PS_SwapChain psSwapchain{ psDevice, psWindow.getExtent() };
+
+		std::unique_ptr<PS_Pipeline> psPipeline;
+		VkPipelineLayout pipelineLayout;
+		std::vector<VkCommandBuffer> commandBuffers;
 	};
 }
