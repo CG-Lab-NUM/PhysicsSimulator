5d2e3cf92ee0e09f4a8b9ffaba2fd32664648475
khosbilegt <89562265+khosbilegt@users.noreply.github.com>
2022-11-30 14:03:42 +0800

    started mesh loader

--- Engine/Headers/PS_Mesh.hpp
+++ Engine/Headers/PS_Mesh.hpp
@@ -1,8 +1,24 @@
-#include "PS_Pipeline.hpp"
+#pragma once
+#include "PS_Generic.hpp"
 
 namespace ps {
 	class PS_Mesh {
 	public:
+		PS_Mesh() {
+			loadObjects();
+		}
+
+		~PS_Mesh() {
+
+		}
+		PS_Mesh(const PS_Mesh&) = delete;
+		PS_Mesh& operator = (const PS_Mesh&) = delete;
+
+		const std::string TEXTURE_PATH;
+		std::vector<std::string> meshPaths;
+		int meshCount = 0;
+
 	private:
+		void loadObjects();
 	};
 }
